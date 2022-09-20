a57beddada7dfb6cffe58e439f13a8ab73ddd500
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-09-20 10:55:14 -0700

    Add script to extract MAMI dataset

---
+++ scripts/extract_raw_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SOURCE_PATH=$1
+
+EXTRACTED_DATA_DIRECTORY=data/extracted
+
+echo Deleing older extracted files at $EXTRACTED_DATA_DIRECTORY
+rm -rf data/extracted
+
+echo "Extracting raw data files"
+mkdir -p $EXTRACTED_DATA_DIRECTORY
+unzip $SOURCE_PATH/training.zip -d $EXTRACTED_DATA_DIRECTORY > logs/mami_training_extraction.log
+unzip -P *MaMiSemEval2022! $SOURCE_PATH/test.zip -d $EXTRACTED_DATA_DIRECTORY > logs/mami_test_extraction.log
