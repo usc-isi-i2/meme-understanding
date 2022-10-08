40e7cddc97113fa4b149b1c0b194e7015a945f0c
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-10-07 23:18:31 -0700

    Don't fail and save failed_images to objects info file

--- src/runner/detr_object_detection.py
+++ src/runner/detr_object_detection.py
@@ -35,30 +35,36 @@ if __name__ == '__main__':
     os.makedirs(detected_object_directory)
 
     detected_objects_info = {}
+    failed_images = []
     detected_object_index = 0
     for sample in tqdm(train_dataset + test_dataset):
-        image = Image.open(sample['input']['image'])
-        inputs = feature_extractor(images=image, return_tensors="pt").to(args.device)
-        outputs = model(**inputs)
+        try:
+            image = Image.open(sample['input']['image'])
+            inputs = feature_extractor(images=image, return_tensors="pt").to(args.device)
+            outputs = model(**inputs)
 
-        target_sizes = torch.tensor([image.size[::-1]]).to(args.device)
-        results = feature_extractor.post_process(outputs, target_sizes=target_sizes)[0]
+            target_sizes = torch.tensor([image.size[::-1]]).to(args.device)
+            results = feature_extractor.post_process(outputs, target_sizes=target_sizes)[0]
 
-        objects_for_current_sample = []
-        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-            box = [round(i, 2) for i in box.tolist()]
-            # let's only keep detections with score > 0.9
-            if score > 0.9:
-                detected_object_index += 1
-                ni = image.crop(box)
-                detected_object_filepath = f'{detected_object_directory}/{detected_object_index}.jpg'
-                ni.save(detected_object_filepath)
-                objects_for_current_sample.append({
-                    'filename': detected_object_filepath,
-                    'confidence': round(score.item(), 3)
-                })
-
-        detected_objects_info[sample['input']['image']] = objects_for_current_sample
+            objects_for_current_sample = []
+            for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+                box = [round(i, 2) for i in box.tolist()]
+                # let's only keep detections with score > 0.9
+                if score > 0.9:
+                    detected_object_index += 1
+                    ni = image.crop(box)
+                    detected_object_filepath = f'{detected_object_directory}/{detected_object_index}.jpg'
+                    ni.save(detected_object_filepath)
+                    objects_for_current_sample.append({
+                        'filename': detected_object_filepath,
+                        'confidence': round(score.item(), 3)
+                    })
+            
+            detected_objects_info[sample['input']['image']] = objects_for_current_sample
+        
+        except:
+            failed_images.append(sample['input']['image'])
     
+    detected_objects_info['failed'] = failed_images
     with open(os.path.join(configs.processed_data_path, configs.objects_info_filename), 'w') as f:
         json.dump(detected_objects_info, f)
